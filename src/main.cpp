817fd1c575d162d75f6a5f894993a7ac4eaf4aa8
Nicholas Baron <nicholas.baron.ten@gmail.com>
2019-10-04 04:09:30 -0700

    Removed comments

--- src/main.cpp
+++ src/main.cpp
@@ -3,21 +3,29 @@
 
 #include <fstream>
 #include <iostream>
+#include <map>
 #include <string>
 #include <vector>
 
-std::vector<std::string> read_file(const std::string& name){
-	
+std::vector<std::string> read_file(const std::string & name) {
 	std::vector<std::string> to_ret{};
 
 	std::ifstream file{name};
-	while(file.good()){
+	while (file.good()) {
 		std::string temp;
 		std::getline(file, temp);
 		to_ret.emplace_back(std::move(temp));
 	}
 
 	return to_ret;
+}
+
+std::vector<std::string> remove_comments(std::vector<std::string> && lines) {
+	for (auto & line : lines) {
+		line = line.substr(0, line.find_first_of(';'));
+	}
+
+	return std::move(lines);
 }
 
 int main(int arg_count, char ** args) {
@@ -27,9 +35,9 @@ int main(int arg_count, char ** args) {
 
 	std::cout << "Reading " << options->input_file << " ..." << std::endl;
 
-	auto raw_input = read_file(options->input_file);
+	auto input = read_file(options->input_file);
 
-	for(const auto& line : raw_input){
-		std::cout << line << std::endl;
-	}
+	input = remove_comments(std::move(input));
+
+	for (const auto & line : input) { std::cout << line << std::endl; }
 }
