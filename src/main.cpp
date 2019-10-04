553ba045911d1a3b1c1ecd3108187651529bed1d
Nicholas Baron <nicholas.baron.ten@gmail.com>
2019-10-04 02:27:50 -0700

    Called command line parse

--- src/main.cpp
+++ src/main.cpp
@@ -4,5 +4,9 @@
 #include <iostream>
 
 int main(int arg_count, char ** args) {
+	auto options = parse_command_line(arg_count, args);
 
+	if (options->show_help) { return 0; }
+
+	std::cout << "Reading " << options->input_file << " ..." << std::endl;
 }
