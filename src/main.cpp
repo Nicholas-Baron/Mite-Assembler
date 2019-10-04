75f9fd26a26ae8d15a0019d3c85eca5f48aa41a8
Nicholas Baron <nicholas.baron.ten@gmail.com>
2019-10-04 04:57:48 -0700

    Cleaned out all human comforts

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 
 #include "cmdargs.hpp"
 
+#include <algorithm>
 #include <fstream>
 #include <iostream>
 #include <map>
@@ -20,9 +21,31 @@ std::vector<std::string> read_file(const std::string & name) {
 	return to_ret;
 }
 
-std::vector<std::string> remove_comments(std::vector<std::string> && lines) {
+// Remove empty lines, comments,
+std::vector<std::string> cleanup_lines(std::vector<std::string> && lines) {
+
+	static constexpr const char * whitespace = " \t\n\v\f";
+
+	// remove empty lines
+	// Check if there is at least one empty line to remove
+	auto to_remove = std::find_if(lines.begin(), lines.end(), [](const auto& item){ return item.empty(); });
+	while(to_remove != lines.end()){
+		// Remove that element and start looking from the element after the removed
+		to_remove = std::find_if(lines.erase(to_remove), lines.end(), [](const auto& item){ return item.empty(); });
+	}
+
 	for (auto & line : lines) {
-		line = line.substr(0, line.find_first_of(';'));
+		line = line.substr(0, line.find_first_of(';')); // delete comments
+		line = line.substr(line.find_first_not_of(whitespace)); // remove left side whitespace
+		line = line.substr(0, line.find_last_not_of(whitespace) + 1); // remove right side whitespace
+	}
+
+	// remove empty lines after doing the other processing (could have generated some other lines)
+	// Check if there is at least one empty line to remove
+	to_remove = std::find_if(lines.begin(), lines.end(), [](const auto& item){ return item.empty(); });
+	while(to_remove != lines.end()){
+		// Remove that element and start looking from the element after the removed
+		to_remove = std::find_if(lines.erase(to_remove), lines.end(), [](const auto& item){ return item.empty(); });
 	}
 
 	return std::move(lines);
