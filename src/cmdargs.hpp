a4698817c03a6af6a493d4f0effe6f8a59afd898
Nicholas Baron <nicholas.baron.ten@gmail.com>
2019-10-04 02:27:22 -0700

    Formatting pass

--- src/cmdargs.hpp
+++ src/cmdargs.hpp
@@ -1,18 +1,18 @@
 #ifndef _CMD_ARGS_
 #define _CMD_ARGS_
 
-#include <memory>	// unique_ptr
+#include <memory>  // unique_ptr
 #include <string>
 
 // Header to interface with clara
 
 // A struct which contains every command line argument
-struct cmd_args{
-	std::string input_file;		// source file
-	std::string output_file;	// destination file
+struct cmd_args {
+	std::string input_file{};   // source file
+	std::string output_file{};  // destination file
+	bool		show_help = false;	// Whether help was printed
 };
 
-std::unique_ptr<cmd_args> parse_command_line(int arg_count, char** args);
+std::unique_ptr<cmd_args> parse_command_line(int arg_count, char ** args);
 
 #endif
-
