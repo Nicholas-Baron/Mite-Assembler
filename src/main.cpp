84ab5e180279ac7b354bca97f243740bf67b2785
Nicholas Baron <nicholas.baron.ten@gmail.com>
2019-10-06 10:21:29 -0700

    Formatting pass

--- src/main.cpp
+++ src/main.cpp
@@ -26,29 +26,39 @@ std::vector<std::string> read_file(const std::string & name) {
 // It takes ownership of its parameters and does some modification over them
 // Remove empty lines, comments,
 std::vector<std::string> cleanup_lines(std::vector<std::string> && lines) {
-
 	static constexpr const char * whitespace = " \t\n\v\f";
 
 	// remove empty lines
 	// Check if there is at least one empty line to remove
-	auto to_remove = std::find_if(lines.begin(), lines.end(), [](const auto& item){ return item.empty(); });
-	while(to_remove != lines.end()){
-		// Remove that element and start looking from the element after the removed
-		to_remove = std::find_if(lines.erase(to_remove), lines.end(), [](const auto& item){ return item.empty(); });
+	auto to_remove
+		= std::find_if(lines.begin(), lines.end(),
+					   [](const auto & item) { return item.empty(); });
+	while (to_remove != lines.end()) {
+		// Remove that element and start looking from the element after the
+		// removed
+		to_remove
+			= std::find_if(lines.erase(to_remove), lines.end(),
+						   [](const auto & item) { return item.empty(); });
 	}
 
 	for (auto & line : lines) {
-		line = line.substr(0, line.find_first_of(';')); // delete comments
-		line = line.substr(line.find_first_not_of(whitespace)); // remove left side whitespace
-		line = line.substr(0, line.find_last_not_of(whitespace) + 1); // remove right side whitespace
+		line = line.substr(0, line.find_first_of(';'));  // delete comments
+		line = line.substr(
+			line.find_first_not_of(whitespace));  // remove left side whitespace
+		line = line.substr(0, line.find_last_not_of(whitespace)
+								  + 1);  // remove right side whitespace
 	}
 
-	// remove empty lines after doing the other processing (could have generated some other lines)
-	// Check if there is at least one empty line to remove
-	to_remove = std::find_if(lines.begin(), lines.end(), [](const auto& item){ return item.empty(); });
-	while(to_remove != lines.end()){
-		// Remove that element and start looking from the element after the removed
-		to_remove = std::find_if(lines.erase(to_remove), lines.end(), [](const auto& item){ return item.empty(); });
+	// remove empty lines after doing the other processing (could have generated
+	// some other lines) Check if there is at least one empty line to remove
+	to_remove = std::find_if(lines.begin(), lines.end(),
+							 [](const auto & item) { return item.empty(); });
+	while (to_remove != lines.end()) {
+		// Remove that element and start looking from the element after the
+		// removed
+		to_remove
+			= std::find_if(lines.erase(to_remove), lines.end(),
+						   [](const auto & item) { return item.empty(); });
 	}
 
 	return std::move(lines);
@@ -65,27 +75,23 @@ int main(int arg_count, char ** args) {
 
 	input = cleanup_lines(std::move(input));
 
-	if(raw_instructions(input)){
+	if (raw_instructions(input)) {
 		std::cout << "Found a raw program, no formatting needed." << std::endl;
-		
-		for(const auto& line : input){
-			std::cout << line << std::endl;
-		}
+
+		for (const auto & line : input) { std::cout << line << std::endl; }
 
 		return 0;
 	}
 
 	auto prog_data = parse(std::move(input));
 
-	for(const auto& var : prog_data.memory){
-		std::cout << var << std::endl;
-	}
+	for (const auto & var : prog_data.memory) { std::cout << var << std::endl; }
 
-	for(const auto & inst : prog_data.instructions){
+	for (const auto & inst : prog_data.instructions) {
 		std::cout << inst << std::endl;
 	}
 
-	for(const auto & label : prog_data.labels){
+	for (const auto & label : prog_data.labels) {
 		std::cout << label.first << "->" << label.second << std::endl;
 	}
 }
--- src/program_data.cpp
+++ src/program_data.cpp
@@ -2,23 +2,22 @@
 #include "program_data.hpp"
 
 #include <cctype>
-#include <iostream> // cerr
+#include <iostream>  // cerr
 
 // this function first splits the lines between memory and instruction types
 // secondly, it generates the labels table
-program_data parse(std::vector<std::string>&& raw_program){
-	
+program_data parse(std::vector<std::string> && raw_program) {
 	program_data to_ret{};
 
 	// separate each line into either a memory or instruction line
-	for(auto&& line : raw_program){
-		if(line.at(0) == '['){
+	for (auto && line : raw_program) {
+		if (line.at(0) == '[') {
 			// memory type line
 			to_ret.memory.push_back(line);
-		}else if(isalnum(line.at(0))){
+		} else if (isalnum(line.at(0))) {
 			// instruction type
 			to_ret.instructions.push_back(line);
-		}else if(line.at(0) != '"'){
+		} else if (line.at(0) != '"') {
 			// unusual line
 			std::cerr << "Unusual line found: " << line << std::endl;
 		}
@@ -26,37 +25,35 @@ program_data parse(std::vector<std::string>&& raw_program){
 
 	// populate the label table
 	// first the variables
-	for(const auto& variable : to_ret.memory){
+	for (const auto & variable : to_ret.memory) {
 		// find the name and remove all initial whitespace
 		auto name = variable.substr(variable.find(']') + 1);
-		while(isspace(name.at(0))){
-			name = name.substr(1);
-		}
+		while (isspace(name.at(0))) { name = name.substr(1); }
 
 		// get the number
 		const auto number_start = variable.find_first_of("0123456789");
-		const auto number_len = variable.find(']') - number_start;
+		const auto number_len   = variable.find(']') - number_start;
 		auto number = std::stoi(variable.substr(number_start, number_len));
 
 		std::cout << "Found label " << name << std::endl;
 
 		auto iter = to_ret.labels.find(name);
-		if(iter == to_ret.labels.end()){
+		if (iter == to_ret.labels.end()) {
 			to_ret.labels.emplace(name, number);
-		}else{
+		} else {
 			// duplicate name
-			std::cerr << "Duplicate label found: " 
-					  << iter->first << " at " << iter->second
-					  << " and " << name <<" at " << number << std::endl;
+			std::cerr << "Duplicate label found: " << iter->first << " at "
+					  << iter->second << " and " << name << " at " << number
+					  << std::endl;
 		}
 	}
 
 	// now the line labels
 	// these need numbers
 	size_t number = 0;
-	for(const auto& line : to_ret.instructions){
+	for (const auto & line : to_ret.instructions) {
 		auto label_end = line.find(':');
-		if(label_end == std::string::npos){
+		if (label_end == std::string::npos) {
 			// continue looking at other lines
 			number++;
 			continue;
@@ -68,13 +65,13 @@ program_data parse(std::vector<std::string>&& raw_program){
 		std::cout << "Found label " << label << std::endl;
 
 		auto iter = to_ret.labels.find(label);
-		if(iter == to_ret.labels.end()){
+		if (iter == to_ret.labels.end()) {
 			to_ret.labels.emplace(label, number);
-		}else{
+		} else {
 			// duplicate name
-			std::cerr << "Duplicate label found: " 
-					  << iter->first << " at " << iter->second
-					  << " and " << label <<" at " << number << std::endl;
+			std::cerr << "Duplicate label found: " << iter->first << " at "
+					  << iter->second << " and " << label << " at " << number
+					  << std::endl;
 		}
 
 		number++;
@@ -84,7 +81,4 @@ program_data parse(std::vector<std::string>&& raw_program){
 }
 
 // this function substitutes the labels as needed
-std::vector<std::string> assemble(program_data&& program){
-	return {};
-}
-
+std::vector<std::string> assemble(program_data && program) { return {}; }
