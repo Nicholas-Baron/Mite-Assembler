e64c252f6a2bf2de9707ab6a2c9436cc3c43aa02
Nicholas Baron <nicholas.baron.ten@gmail.com>
2019-10-06 18:44:01 -0700

    fixed the debug flag

--- src/cmdargs.cpp
+++ src/cmdargs.cpp
@@ -13,8 +13,7 @@ std::unique_ptr<cmd_args> parse_command_line(int arg_count, char ** args) {
 			   | clara::Arg(to_ret.input_file, "input file")("File to read")
 			   | clara::Opt(to_ret.output_file,
 							"output file")["-o", "--output"]("File to write to")
-			   | clara::Opt(to_ret.show_debug,
-							"show debug")["-v"]("Show the debug printing");
+			   | clara::Opt(to_ret.show_debug)["-v"]("Show the debug printing");
 
 	auto result = cli.parse(clara::Args(arg_count, args));
 
